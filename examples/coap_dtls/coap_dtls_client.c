cf0fedb31a42fb9fbd86e943cf67a01b95cbf8e8
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2019-12-11 12:55:36 +0100

    psk ciphersuite added to adtls 1.2

--- examples/coap_dtls/coap_dtls_client.c
+++ examples/coap_dtls/coap_dtls_client.c
@@ -39,7 +39,7 @@
 #endif
 
 static int config_index = 0;
-static char *config[] = {"PSK", "ECDHE-ECDSA-AES128-CCM-8", "ECDHE-ECDSA-AES256-CCM-8"};
+static char *config[] = {"PSK-AES256-CCM-8", "ECDHE-ECDSA-AES128-CCM-8", "ECDHE-ECDSA-AES256-CCM-8"};
 
 /* identity is OpenSSL testing default for openssl s_client, keep same */
 static const char* kIdentityStr = "Client_identity";
@@ -282,19 +282,20 @@ WOLFSSL* Client(WOLFSSL_CTX* ctx, char* suite, int setSuite, int doVerify)
 
         //TODO: to be refined
 
-        config_index = 2;
-        if (( ret = wolfSSL_CTX_set_cipher_list(ctx, config[config_index])) != SSL_SUCCESS) {
-            printf("ret = %d\n", ret);
-            printf("Error :can't set cipher\n");
-            wolfSSL_CTX_free(ctx);
-            return NULL;
-        }
+        config_index = 1;
         
     #endif
 
 #else /* !def MODULE_WOLFSSL_PSK */
     wolfSSL_CTX_set_psk_client_callback(ctx, my_psk_client_cb);
 #endif
+
+    if (( ret = wolfSSL_CTX_set_cipher_list(ctx, config[config_index])) != SSL_SUCCESS) {
+            printf("ret = %d\n", ret);
+            printf("Error :can't set cipher\n");
+            wolfSSL_CTX_free(ctx);
+            return NULL;
+    }
 
     wolfSSL_SetIORecv(ctx, client_recv);
     wolfSSL_SetIOSend(ctx, client_send);
