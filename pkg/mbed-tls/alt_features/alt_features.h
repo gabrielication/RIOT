ceec9a3e598a59db19c9f952347c108bb3dd9d34
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2020-04-30 18:18:36 +0200

    prng support added (bugfix for boards without hwrng)

--- pkg/mbed-tls/alt_features/alt_features.h
+++ pkg/mbed-tls/alt_features/alt_features.h
@@ -1,4 +1,8 @@
 #include <stddef.h>
+#include "random.h"
+
+#ifdef MODULE_PERIPH_HWRNG
 #include "periph/hwrng.h"
+#endif
 
 int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen);
