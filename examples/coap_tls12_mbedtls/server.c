d03a4aa97598de5696614b644e113146b0250320
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2020-06-25 14:30:05 +0200

    examples renamed. bug fixed on server atls 1.2.

--- examples/coap_tls12_mbedtls/server.c
+++ examples/coap_tls12_mbedtls/server.c
@@ -361,16 +361,7 @@ int mbedtls_server_init(void)
             TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256
             TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384
 **/
-
-    cipher[0] = mbedtls_ssl_get_ciphersuite_id("TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384");
-    cipher[1] = 0;
-
-    if (cipher[0] == 0)
-            {
-                mbedtls_printf("forced ciphersuite not found\n");
-                ret = 2;
-                return ret;
-    }
+    
 /*
     const mbedtls_ssl_ciphersuite_t *ciphersuite_info;
     ciphersuite_info = mbedtls_ssl_ciphersuite_from_id( cipher[0] );
